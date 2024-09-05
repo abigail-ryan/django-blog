86ca633ec10a8cf9f3177ba18a2f9e97d86fe89e
Abigail Ryan <abi.ryan91@gmail.com>
2024-09-05 20:21:34 +0000

    Add comments model to database

--- blog/models.py
+++ blog/models.py
@@ -24,6 +24,6 @@ class Comment(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter"
     )
-    body = modles.TextField()
+    body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
