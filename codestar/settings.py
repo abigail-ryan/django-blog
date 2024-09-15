4cb66e29614bc3a6d1f57114864e33186380a1bc
Abigail Ryan <abi.ryan91@gmail.com>
2024-09-15 23:56:56 +0000

    Add user CRUD functionality for comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-abigailryan-djangoblog-mw40xv87ygg.ws.codeinstitute-ide.net', '.herokuapp.com']
 
